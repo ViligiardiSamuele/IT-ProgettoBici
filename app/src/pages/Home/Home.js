a5d4d7fb402c3524480971bd6bfe3e574fb1d90e
ViligiardiSamuele <viligiardi.samuele@itismeucci.com>
2024-05-24 17:35:11 +0200

    wip

--- app/src/pages/Home/Home.js
+++ app/src/pages/Home/Home.js
@@ -4,6 +4,7 @@ import Container from "react-bootstrap/esm/Container.js";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import UserInfo from "../../components/UserInfo/UserInfo.js";
 
 export default function Home() {
   return (
@@ -11,11 +12,13 @@ export default function Home() {
       <NavbarTop />
       <Container>
         <Row>
-          <Col sm={8} className="p-2">
+          <Col sm={12} className="p-2">
             <Card className="p-2">
-              <p>1</p>
+              <UserInfo />
             </Card>
           </Col>
+        </Row>
+        <Row>
           <Col sm={4} className="p-2">
             <Card className="p-2">
               <p>2</p>
