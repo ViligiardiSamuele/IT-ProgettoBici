8e8bcc1fb450904b1c6ca10f6a3e0ffb8bc724ef
iVillo04 <viligiardisamuele@gmail.com>
2024-07-01 20:11:25 +0200

    wip

--- build/init.sql
+++ build/init.sql
@@ -26,8 +26,8 @@ CREATE TABLE
     id_gara INT NOT NULL, -- pk
     id_utente INT NOT NULL, -- pk
     PRIMARY KEY (id_gara, id_utente),
-    FOREIGN KEY (id_gara) REFERENCES Gare (id_gara) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (id_utente) REFERENCES Utenti (id_utente) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (id_gara) REFERENCES Gare(id_gara) ON DELETE CASCADE,
+    FOREIGN KEY (id_utente) REFERENCES Utenti(id_utente) ON DELETE CASCADE
   ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = 'Tabella degli organizzatori delle gare';
 
 CREATE TABLE
@@ -36,69 +36,6 @@ CREATE TABLE
     id_utente INT NOT NULL, -- pk
     tempoInGara INT DEFAULT 0,
     PRIMARY KEY (id_gara, id_utente),
-    FOREIGN KEY (id_gara) REFERENCES Gare (id_gara) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (id_utente) REFERENCES Utenti (id_utente) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (id_gara) REFERENCES Gare(id_gara) ON DELETE CASCADE,
+    FOREIGN KEY (id_utente) REFERENCES Utenti(id_utente) ON DELETE CASCADE
   ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = 'Tabella dei concorrenti nelle gare';
-
--- Inserimento di dati nella tabella Utenti
-INSERT INTO
-  Utenti (nome, cognome, nascita, email, psw)
-VALUES
-  (
-    'Mario',
-    'Rossi',
-    '1985-01-01',
-    'mario.rossi@example.com',
-    SHA2 ('password1', 256)
-  ),
-  (
-    'Luigi',
-    'Verdi',
-    '1990-05-15',
-    'luigi.verdi@example.com',
-    SHA2 ('password2', 256)
-  ),
-  (
-    'Anna',
-    'Bianchi',
-    '1975-09-10',
-    'anna.bianchi@example.com',
-    SHA2 ('password3', 256)
-  ),
-  (
-    'Giulia',
-    'Neri',
-    '2000-12-20',
-    'giulia.neri@example.com',
-    SHA2 ('password4', 256)
-  );
-
--- Inserimento di dati nella tabella Gare
-INSERT INTO
-  Gare (nome, maxConcorrenti, minEta, chiusa)
-VALUES
-  ('Gara Lunga', 100, 50, FALSE),
-  ('Gara Media', -1, 16, TRUE),
-  ('Gara Corta', 10, -1, FALSE),
-
--- Inserimento di dati nella tabella Organizzatori
-INSERT INTO
-  Organizzatori (id_gara, id_utente)
-VALUES
-  (1, 1),
-  (1, 2),
-  (2, 3),
-  (3, 4);
-
--- Inserimento di dati nella tabella Concorrenti
-INSERT INTO
-  Concorrenti (id_gara, id_utente, tempoInGara)
-VALUES
-  (1, 3, 3600),
-  (1, 4, 3700),
-  (2, 1, 1800),
-  (2, 2, 1900),
-  (3, 1, 1200),
-  (3, 2, 1300),
-  (4, 3, 2400),
-  (4, 4, 2500);
